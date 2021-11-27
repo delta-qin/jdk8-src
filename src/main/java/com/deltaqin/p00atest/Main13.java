d3c8ea8d3a5a6a3d9c8b727f96e3fe9223dbd0a2
deltaqin <delta_qin@163.com>
2021-11-27 16:44:16 +0800

    近期的一些小练习

---
+++ src/main/java/com/deltaqin/p00atest/Main13.java
@@ -0,0 +1,12 @@
+package com.deltaqin.p00atest;
+
+/**
+ * @author deltaqin
+ * @date 2021/11/27 11:41 上午
+ */
+public class Main13 {
+    public static void main(String[] args) {
+        String property = System.getProperty("user.home");
+        System.out.println(property);
+    }
+}
